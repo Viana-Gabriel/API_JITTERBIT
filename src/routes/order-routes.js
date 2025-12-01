a5a3af6471c972f2f427095ebd9b3c4addbc3451
Gabriel Viana Santos <gabriel.viana.gv02@gmail.com>
2025-12-01 14:39:35 -0300

    fix : ajuste na ordem da rota

--- src/routes/order-routes.js
+++ src/routes/order-routes.js
@@ -4,8 +4,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post("/order", OrderController.createOrder);
-router.get("/order/:orderId", OrderController.getOrder);
 router.get("/order/list", OrderController.listOrders);
+router.get("/order/:orderId", OrderController.getOrder);
 router.put("/order/:orderId", OrderController.updateOrder);
 router.delete("/order/:orderId", OrderController.deleteOrder);
 
