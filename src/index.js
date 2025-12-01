a5123a1f551a320717cfe8d1600fb1adebcdb205
Gabriel Viana Santos <gabriel.viana.gv02@gmail.com>
2025-12-01 12:40:28 -0300

    feat : configuraçao do banco e do arquivo principal da api

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from "dotenv";
+import { pool } from "./database/connection.js";
+
+dotenv.config();
+const PORT = process.env.APP_PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// ROTA DE TESTE
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      message: "Conexão bem-sucedida!",
+      serverTime: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao conectar ao banco" });
+  }
+});
+
+app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
